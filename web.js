376ca48dceff36c1f211928aed002df163009aba
Larry Price <larry.price.dev@gmail.com>
2014-08-12 20:15:15 -0400

    moving stuff around and prepping for making teeeeeeeeests

--- web.js
+++ web.js
@@ -1,18 +1,9 @@
 var express = require('express');
-var fortunes = require('./routes/fortunes');
-var lottos = require('./routes/lottos');
-var lessons = require('./routes/lessons');
-var cookie = require('./routes/cookie');
+var routes = require('./lib/routes');
 
 var app = express();
 
-app.get('/v1/fortunes', fortunes.findAll);
-app.get('/v1/fortunes/:id', fortunes.findById);
-app.get('/v1/lottos', lottos.findAll);
-app.get('/v1/lottos/:id', lottos.findById);
-app.get('/v1/lessons', lessons.findAll);
-app.get('/v1/lessons/:id', lessons.findById);
-app.get('/cookie', cookie.build);
+routes.setup(app);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
