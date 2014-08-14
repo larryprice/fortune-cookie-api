7da6e4a8dd1b4166be455c9d6a1db44223703cac
Larry Price <larry.price.dev@gmail.com>
2014-08-13 20:44:09 -0400

    tests actually working

--- test/routes.test.js
+++ test/routes.test.js
@@ -9,7 +9,58 @@ describe("fortune cookie api", function() {
       .expect(200)
       .end(function(err, res) {
         should.not.exist(err);
-        console.log(res);
+        res.body.should.have.deep.members([{
+          message: 'Fortune 1',
+          id: '0'
+        }, {
+          message: 'Fortune 2',
+          id: '1'
+        }, {
+          message: 'Fortune 3',
+          id: '2'
+        }]);
+        done();
+      });
+  });
+
+  it("gets all the lottos", function(done) {
+    request(app)
+      .get("/v1/lottos")
+      .expect(200)
+      .end(function(err, res) {
+        should.not.exist(err);
+        res.body.should.have.deep.members([{
+          id: '1',
+          numbers: [10, 20, 30, 40, 50, 59]
+        }, {
+          id: '2',
+          numbers: [5, 10, 15, 20, 25, 30]
+        }, {
+          id: '3',
+          numbers: [8, 60, 24, 32, 48, 40]
+        }]);
+        done();
+      });
+  });
+
+
+  it("gets all the lessons", function(done) {
+    request(app)
+      .get("/v1/lessons")
+      .expect(200)
+      .end(function(err, res) {
+        should.not.exist(err);
+        res.body.should.have.deep.members([{
+          "id": 1,
+          "chinese": "因特网",
+          "romanization": "yintewang",
+          "english": "internet"
+        }, {
+          "id": 2,
+          "chinese": "狮子狗",
+          "romanization": "shizi gou",
+          "english": "poodle"
+        }]);
         done();
       });
   });
