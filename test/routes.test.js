520de5b735a5bbf571ee595108ca4831bed50de0
Larry Price <larry.price.dev@gmail.com>
2014-08-31 19:42:36 -0400

    Lotto methods - findAll only implemented for first 10, which is kind of lame

--- test/routes.test.js
+++ test/routes.test.js
@@ -63,21 +63,42 @@ describe("fortune cookie api", function() {
   });
 
   describe("lottos", function() {
-    it("gets all the lottos", function(done) {
+    it("gets all the lottos with default limit", function(done) {
       request(app)
         .get("/v1/lottos")
         .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
           res.body.should.have.deep.members([{
-            id: '1',
-            numbers: [10, 20, 30, 40, 50, 59]
+            id: '000000000000000000000000',
+            numbers: [0, 0, 0, 0, 0, 0]
           }, {
-            id: '2',
-            numbers: [5, 10, 15, 20, 25, 30]
+            id: '000000000000000000000001',
+            numbers: [0, 0, 0, 0, 0, 1]
           }, {
-            id: '3',
-            numbers: [8, 60, 24, 32, 48, 40]
+            id: '000000000000000000000002',
+            numbers: [0, 0, 0, 0, 0, 2]
+          }, {
+            id: '000000000000000000000003',
+            numbers: [0, 0, 0, 0, 0, 3]
+          }, {
+            id: '000000000000000000000004',
+            numbers: [0, 0, 0, 0, 0, 4]
+          }, {
+            id: '000000000000000000000005',
+            numbers: [0, 0, 0, 0, 0, 5]
+          }, {
+            id: '000000000000000000000006',
+            numbers: [0, 0, 0, 0, 0, 6]
+          }, {
+            id: '000000000000000000000007',
+            numbers: [0, 0, 0, 0, 0, 7]
+          }, {
+            id: '000000000000000000000008',
+            numbers: [0, 0, 0, 0, 0, 8]
+          }, {
+            id: '000000000000000000000009',
+            numbers: [0, 0, 0, 0, 0, 9]
           }]);
           done();
         });
@@ -85,13 +106,13 @@ describe("fortune cookie api", function() {
 
     it("gets requested lotto", function(done) {
       request(app)
-        .get("/v1/lottos/1")
+        .get("/v1/lottos/002500460007001100580026")
         .expect(200)
         .end(function(err, res) {
           should.not.exist(err);
           res.body.should.deep.equal({
-            numbers: [10, 20, 30, 40, 50, 59],
-            id: '1'
+            id: "002500460007001100580026",
+            numbers: [25, 46, 7, 11, 58, 26]
           });
           done();
         });
@@ -112,6 +133,7 @@ describe("fortune cookie api", function() {
         english: "poodle"
       });
     });
+
     it("gets all the lessons", function(done) {
       request(app)
         .get("/v1/lessons")
